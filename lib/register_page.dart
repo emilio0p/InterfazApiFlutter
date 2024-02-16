47139d2440ab5efdaa1e85d4b9e1f39955b641aa
Emilio <emilio.ordunap@hotmail.com>
2024-02-16 01:08:49 +0100

    v2

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -1,13 +1,39 @@
+import 'package:api_interface/login_page.dart';
 import 'package:flutter/material.dart';
 import 'auth_service.dart';
 import 'RegisterRequest.dart';
+import 'register_page.dart';
+import 'HomePage.dart';
 
-class RegisterPage extends StatelessWidget {
-  final AuthService authService = AuthService();
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final AuthService _authService = AuthService();
   final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
+
+  Future<void> _register() async {
+    try {
+      await _authService.register(RegisterRequest(
+        username: _usernameController.text,
+        email: _emailController.text,
+        password: _passwordController.text,
+        description: _descriptionController.text,
+      ));
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()), // Redirige al usuario a la página de inicio después del registro exitoso
+      );
+    } catch (error) {
+      // Maneja el error si el registro falla
+      print('Error en el registro: $error');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,58 +41,45 @@ class RegisterPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Registro'),
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                controller: _usernameController,
-                decoration: InputDecoration(
-                  labelText: 'Nombre de usuario',
-                ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: 'Nombre de usuario',
               ),
-              SizedBox(height: 20.0),
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: 'Correo electrónico',
-                ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Correo electrónico',
               ),
-              SizedBox(height: 20.0),
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Contraseña',
-                ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Contraseña',
               ),
-              SizedBox(height: 20.0),
-              TextField(
-                controller: _descriptionController,
-                decoration: InputDecoration(
-                  labelText: 'Descripción',
-                ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              controller: _descriptionController,
+              decoration: InputDecoration(
+                labelText: 'Descripción',
               ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  authService.register(RegisterRequest(
-                    username: _usernameController.text,
-                    email: _emailController.text,
-                    password: _passwordController.text,
-                    description: _descriptionController.text,
-                  )).then((response) {
-                    // Manejar la respuesta del registro
-                  }).catchError((error) {
-                    // Manejar errores
-                  });
-                },
-                child: Text('Registrarse'),
-              ),
-            ],
-          ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: _register,
+              child: Text('Registrarse'),
+            ),
+          ],
         ),
       ),
     );
